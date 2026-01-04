a478035ecd8783c32be6a3ffa74b0313a38b4f30
yabu1121 <n.hayato0122@outlook.jp>
2026-01-04 23:31:56 +0900

    433

--- ABC433.cpp
+++ ABC433.cpp
@@ -4,6 +4,18 @@ using namespace std;
 void A(){
   int x,y,z;
   cin >> x >> y >> z;
+  bool flag = false;
+  while(x < 100 && y < 100){
+    if(x == y * z) {
+      flag = true;
+      break;
+    };
+    x++;
+    y++;
+  }
+
+  if(flag)cout << "Yes" << endl;
+  else cout << "No" << endl;
 }
 
 int main(){
