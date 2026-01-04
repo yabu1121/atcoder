dd3364a9ecc3c7289a78be03af48c998e90376c0
yabu1121 <n.hayato0122@outlook.jp>
2026-01-04 23:24:01 +0900

    432

--- ABC432.cpp
+++ ABC432.cpp
@@ -4,8 +4,34 @@ using namespace std;
 void A(){
   int a,b,c;
   cin >> a >> b >> c;
-  
-
+  int maxint = max(max(a,b),c);
+  int minint = min(min(a,b),c);
+  int center;
+  if (a == maxint){
+    if(b == minint){
+      center = c;
+    }
+    if(c == minint){
+      center = b;
+    }
+  }
+  if (b == maxint){
+    if(a == minint){
+      center = c;
+    }
+    if(c == minint){
+      center = a;
+    }
+  }
+  if (c == maxint){
+    if(b == minint){
+      center = a;
+    }
+    if(a == minint){
+      center = b;
+    }
+  }
+  cout << maxint << center << minint << endl;
 }
 
 int main(){
