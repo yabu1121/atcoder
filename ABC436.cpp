7518bd0e364caea487df3d59f06fdedea801826c
yabu1121 <n.hayato0122@outlook.jp>
2026-01-04 23:51:29 +0900

    436

--- ABC436.cpp
+++ ABC436.cpp
@@ -2,7 +2,13 @@
 using namespace std;
 
 void A(){
-
+  int n;
+  string s;
+  cin >> n >> s;
+  for(int i = 0; i < n - s.length(); i++){
+    cout << "o";
+  }
+  cout << s << endl;
 }
 
 int main(){
