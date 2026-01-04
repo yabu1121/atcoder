701f96fe9ec5559d1e9d9e2fa9d07721134bfd0c
yabu1121 <n.hayato0122@outlook.jp>
2026-01-04 23:48:14 +0900

    435

--- ABC435.cpp
+++ ABC435.cpp
@@ -1,8 +1,15 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+int sigma(int num){
+  if(num == 1)return 1;
+  return num + sigma(num - 1);
+}
+
 void A(){
-  
+  int n;
+  cin >> n;
+  cout << sigma(n) << endl;
 }
 
 int main(){
