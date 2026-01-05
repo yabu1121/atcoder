c98341e87b968e016cf95f40921350bc968d0b45
yabu1121 <n.hayato0122@outlook.jp>
2026-01-05 20:24:27 +0900

    438

--- ABC438.cpp
+++ ABC438.cpp
@@ -2,7 +2,18 @@
 using namespace std;
 
 void A(){
-  
+  int d,f;
+  cin >> d >> f;
+  int n = 0;
+  int result;
+  while(true){
+    if( f + 7 * n > d ){
+      result = f + 7 * n - d;
+      break;
+    }
+    n++;
+  }
+  cout << result << endl;
 }
 
 int main(){
