726c993cefdc8927a45b58e5e3c7c19baba2b493
yabu1121 <n.hayato0122@outlook.jp>
2026-01-13 00:29:15 +0900

    abc 431b

--- ABC430.cpp
+++ ABC430.cpp
@@ -11,7 +11,7 @@ void A(){
 }
 
 void B(){
-  
+
 }
 
 int main(){
--- ABC431.cpp
+++ ABC431.cpp
@@ -12,10 +12,38 @@ void A(){
 
 
 void B(){
-  
+  int x, n;
+  cin >> x >> n;
+  vector<int> w(n);
+  for(int i = 0; i < n; i++){
+    cin >> w.at(i);
+  }
+  int q;
+  cin >> q;
+  vector<int> p(q);
+  for(int i = 0; i < q; i++){
+    cin >> p.at(i);
+  }
+
+  vector<bool> attach(n);
+  for(int i = 0; i < n; i++){
+    attach.at(i) = false;
+  }
+
+  for(int i = 0; i < q; i++){
+    int num = p.at(i) - 1;
+    if(attach.at(num)){
+      x -= w.at(num);
+      attach.at(num) = false;
+    }else{
+      x += w.at(num);
+      attach.at(num) = true;
+    }
+    cout << x << endl;
+  }
 }
 
 int main(){
-  A();
+  B();
   return 0;
 }
